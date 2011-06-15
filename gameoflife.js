721b613684e5cc53a4760e16414515e55b56b4fc
rozuur <rozuur@gmail.com>
2011-06-15 23:44:33 +0530

    added buttons play and pause

--- gameoflife.js
+++ gameoflife.js
@@ -8,6 +8,7 @@ var kPixelHeight= 1 + (kBoardHeight * kPieceHeight);
 var gCanvas;
 var gContext;
 var gPieces;
+var gTimer;
 
 function Cell(row, column) {
     this.row = row;
@@ -23,43 +24,43 @@ var clicks = 0;
 function clickCell(cell){
     var column = cell.column;
     var row = cell.row;
-	gPieces[row * kBoardWidth + column] = 1;
-	drawCell(column,row);
-	clicks++;
-	if(clicks == 3){
-		//var timerId = setInterval(animateLife,100);
-	}
+    gPieces[row * kBoardWidth + column] = 1;
+    drawCell(column,row);
+    clicks++;
+    if(clicks == 3){
+	//var timerId = setInterval(animateLife,100);
+    }
 }
 
 function drawCell(x,y){
-	x *= (kPieceWidth);
+    x *= (kPieceWidth);
     y *= (kPieceHeight);
-	gContext.fillRect(x,y,kPieceWidth,kPieceHeight);
+    gContext.fillRect(x,y,kPieceWidth,kPieceHeight);
 }
 
 function drawGeneration(){
-	pieces = gPieces;
-	drawBoard();
-	for(var r = 0, h = kBoardHeight; r < h; ++r){
-		for(var c = 0, w = kBoardWidth; c < w; ++c){
-			if(pieces[r * kBoardWidth + c]){
-				drawCell(c,r);
-			}
-		}
+    var pieces = gPieces;
+    drawBoard();
+    for(var r = 0, h = kBoardHeight; r < h; ++r){
+	for(var c = 0, w = kBoardWidth; c < w; ++c){
+	    if(pieces[r * kBoardWidth + c]){
+		drawCell(c,r);
+	    }
 	}
+    }
 }
 
 function idx(r,c){
-	return r * kBoardWidth + c;
+    return r * kBoardWidth + c;
 }
 
 
 function valAt(r,c){
-	if(gPieces[r * kBoardWidth + c]){
-		return 1;
-	}else{
-		return 0;
-	}
+    if(gPieces[r * kBoardWidth + c]){
+	return 1;
+    }else{
+	return 0;
+    }
 }
 
 function nextGeneration(){
@@ -100,19 +101,19 @@ function getCursorPosition(e) {
 
 function drawBoard() {
     gContext.clearRect(0, 0, kPixelWidth, kPixelHeight);
-	setTimeout(null,1000);
+    setTimeout(null,1000);
     gContext.beginPath();
     
     /* vertical lines */
     for (var x = 0; x <= kPixelWidth; x += kPieceWidth) {
-		gContext.moveTo(0.5 + x, 0);
-		gContext.lineTo(0.5 + x, kPixelHeight);
-	}
+	gContext.moveTo(0.5 + x, 0);
+	gContext.lineTo(0.5 + x, kPixelHeight);
+    }
     
     /* horizontal lines */
     for (var y = 0; y <= kPixelHeight; y += kPieceHeight) {
-		gContext.moveTo(0, 0.5 + y);
-		gContext.lineTo(kPixelWidth, 0.5 +  y);
+	gContext.moveTo(0, 0.5 + y);
+	gContext.lineTo(kPixelWidth, 0.5 +  y);
     }
     
     /* draw it! */
@@ -122,32 +123,44 @@ function drawBoard() {
 
 
 function initGame(canvasElement){
-	if(!canvasElement){
-		canvasElement = document.createElement("canvas");
-		canvasElement.Id = "gameoflife_canvas";
-		document.body.appendChild(canvasElement);
-	}
-	
-	gCanvas = canvasElement;
-	gContext = gCanvas.getContext("2d");
+    if(!canvasElement){
+	canvasElement = document.createElement("canvas");
+	canvasElement.Id = "gameoflife_canvas";
+	document.body.appendChild(canvasElement);
+    }
+    
+    gCanvas = canvasElement;
+    gContext = gCanvas.getContext("2d");
     gCanvas.width = kPixelWidth;
     gCanvas.height = kPixelHeight;
     gCanvas.addEventListener("click", lifeOnClick, false);
-	gPieces = new Array(kBoardWidth * kBoardHeight);
-	for(var i = 0, l = gPieces.length; i < l; ++i){
-		gPieces[i] = getRandomInt(0,1);
-	}
-	drawBoard();
+    gPieces = new Array(kBoardWidth * kBoardHeight);
+    for(var i = 0, l = gPieces.length; i < l; ++i){
+	gPieces[i] = getRandomInt(0,1);
+    }
+    drawBoard();
 }
 
 function getRandomInt(min, max)
 {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function pauseGame(){
+    // clear timer
+    clearInterval(gTimer);
+    gTimer = null;
+}
+
+function playGame(){
+    if(!gTimer){
+        gTimer = setInterval(animateLife,100);    
+    }
 }
 
 function animateLife(){
-	nextGeneration();
-	drawGeneration();
+    nextGeneration();
+    drawGeneration();
 }
 
-var timerId = setInterval(animateLife,100);
+
